4631a703c889850998c96f854fb05c9d25bea53b
TIMMCMIC <timmcmic@microsoft.com>
2021-03-03 14:14:09 -0500

    Initial Commit

---
+++ Out-LogFile.ps1
@@ -0,0 +1,21 @@
+Function Out-LogFile {
+    [cmdletbinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]$String
+    )
+
+    # Get our log file path
+    $LogFile = Join-path $env:LOCALAPPDATA ExPefwiz.log
+
+    # Get the current date
+    [string]$date = Get-Date -Format G
+
+    # Build output string
+    [string]$logstring = ( "[" + $date + "] - " + $string)
+
+    # Write everything to our log file and the screen
+    $logstring | Out-File -FilePath $LogFile -Append
+    Write-Verbose  $logstring
+}
