c33a48d910c0e208f8ca0e72f732fca992ee970b
TIMMCMIC <timmcmic@microsoft.com>
2021-03-15 16:30:08 -0400

    Corrected user syntax

--- Get-NormalizedDN.ps1
+++ Get-NormalizedDN.ps1
@@ -146,7 +146,7 @@
 
             #At this point we have users that could be added to managedBy or members that are not mail enabled.  This is permissable through ADUC tools and supported with Exchange commands.
 
-            elseif ($functionTest.objectClass -eq "User"))
+            elseif ($functionTest.objectClass -eq "User")
             {
                 Out-LogFile -string "The object is a user only object hopefully in managedBY or USERS."
                     
