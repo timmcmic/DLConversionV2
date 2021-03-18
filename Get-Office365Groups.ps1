0c83b63fd284ca8cec08ca880456f407f45e3454
TIMMCMIC <timmcmic@microsoft.com>
2021-03-17 20:44:06 -0400

    Ttest

--- Get-Office365Groups.ps1
+++ Get-Office365Groups.ps1
@@ -59,7 +59,7 @@
                 $functionGroups = get-O365DistributionGroup -resultsize unlimited -filter { isDirSynced -eq $FALSE }
             }
             
-            out-logFile -string $functionGroups.count
+            out-logFile -string $functionGroups.count.tostring()
             Out-LogFile -string "All cloud only groups were located."
         }
         catch 
