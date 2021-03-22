9fd4fefe0738e15035b247e6afe51e4e39a5c733
TIMMCMIC <timmcmic@microsoft.com>
2021-03-21 22:44:47 -0400

    Finishing dependency code

--- DLConversionV2.psm1
+++ DLConversionV2.psm1
@@ -1216,7 +1216,7 @@ Function Start-DistributionListMigration
     Out-LogFile -string "START Disable on premises distribution group."
     Out-LogFile -string "********************************************************************************"
 
-    Disable-OriginalDL -dn $originalDLConfiguration.distinguishedName -globalCatalogServer $globalCatalogServer -parameterSet $dlPropertySetToClear
+    #Disable-OriginalDL -dn $originalDLConfiguration.distinguishedName -globalCatalogServer $globalCatalogServer -parameterSet $dlPropertySetToClear
 
     Out-LogFile -string "********************************************************************************"
     Out-LogFile -string "END VALIDATE RECIPIENTS IN CLOUD"
