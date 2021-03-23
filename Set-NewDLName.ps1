c08a0fb2c9680b16073029f2894ed4b9bee01aa5
TIMMCMIC <timmcmic@microsoft.com>
2021-03-23 10:30:02 -0400

    Attempting to manage strings

--- Set-NewDLName.ps1
+++ Set-NewDLName.ps1
@@ -80,7 +80,7 @@
         {
             Out-LogFile -string "Setting the new group SAM Account Name.."
 
-            set-adgroup -identity $dn -samAccountName $functionSAMAccountName -server $globalCatalogServer
+            set-adgroup -identity $dn -samAccountName '$functionSAMAccountName' -server $globalCatalogServer
         }
         catch 
         {
