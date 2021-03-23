3e2e879c26ef352276e3932898d0638a896892df
TIMMCMIC <timmcmic@microsoft.com>
2021-03-23 12:46:54 -0400

    Test DL is present

---
+++ test-CloudDLPresent.ps1
@@ -0,0 +1,64 @@
+<#
+    .SYNOPSIS
+
+    This function loops until we detect that the cloud DL is no longer present.
+    
+    .DESCRIPTION
+
+    This function loops until we detect that the cloud DL is no longer present.
+
+    .PARAMETER groupSMTPAddress
+
+    The SMTP Address of the group.
+
+    .OUTPUTS
+
+    None
+
+    .EXAMPLE
+
+    test-CloudDLPresent -groupSMTPAddress SMTPAddress
+
+    #>
+    Function test-CloudDLPresent
+     {
+        [cmdletbinding()]
+
+        Param
+        (
+            [Parameter(Mandatory = $true)]
+            [string]$groupSMTPAddress
+        )
+
+        #Declare function variables.
+
+        [boolean]$firstLoopProcessing=$TRUE
+
+        #Start function processing.
+
+        Out-LogFile -string "********************************************************************************"
+        Out-LogFile -string "BEGIN TEST-CLOUDDLPRESENT"
+        Out-LogFile -string "********************************************************************************"
+
+        #Log the parameters and variables for the function.
+
+        out-Logfile -string ("Group SMTP Address = "+$groupSMTPAddress)
+
+        do 
+        {
+            if ($firstLoopProcessing -eq $TRUE)
+            {
+                Out-LogFile -string "First time checking for group - do not sleep."
+                $firstLoopProcessing = $FALSE
+            }
+            else 
+            {
+                Out-LogFile -string "Group previously found - sleep 30 seconds and check again."
+                start-sleep -seconds 30
+            }
+
+        } while (get-o365DistributionGroup -identity $groupSMTPAddress)
+
+        Out-LogFile -string "END TEST-CLOUDDLPRESENT"
+        Out-LogFile -string "********************************************************************************"
+    }
