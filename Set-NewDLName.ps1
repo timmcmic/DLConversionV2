5fdd049f9b1bf1a4e4dcb2b443af5d45837bc333
TIMMCMIC <timmcmic@microsoft.com>
2021-03-23 09:40:40 -0400

    Updating attribute name

--- Set-NewDLName.ps1
+++ Set-NewDLName.ps1
@@ -80,7 +80,7 @@
         {
             Out-LogFile -string "Setting the new group SAM Account Name.."
 
-            set-adgroup -identity $dn -sameAccountName $functionSAMAccountName
+            set-adgroup -identity $dn -samAccountName $functionSAMAccountName
         }
         catch 
         {
