25d2117a2bf9487658c7a482d5a867f323168b1d
TIMMCMIC <timmcmic@microsoft.com>
2021-03-23 21:24:53 -0400

    Checking in set code.

--- set-Office365DL.ps1
+++ set-Office365DL.ps1
@@ -39,6 +39,8 @@
         #Declare function variables.
 
         $functionMemberDepartRestrictionType=$NULL #Holds the return information for the group query.
+        $functionModerationFlags=$NULL
+        $functionSendModerationNotifications=$NULL
 
         #Start function processing.
 
@@ -57,17 +59,60 @@
         if ( $groupTypeOverride -eq "Security" )
 		{
 			$functionMemberDepartRestriction = "Closed"
+
+            out-logfile -string ("Function member depart restrictions = "+$functionMemberDepartRestriction)
 		}
 		else 
 		{
 			$functionMemberDepartRestriction = $originalDLConfiguration.msExchGroupDepartRestriction
+
+            out-logfile -string ("Function member depart restrictions = "+$functionMemberDepartRestriction)
 		}
 
+        #The moderation settings a are a hash valued flag.
+
         #Create the distribution group in office 365.
+
+        if (($originalDLConfiguration.msExchModerationFlags -eq "1") -or ($originalDLConfiguration.msExchModerationFlags -eq "3") -or ($originalDLConfiguration.msExchModerationFlags -eq "7") )
+        {
+            out-logfile -string ("The moderation flags are 1 / 3 / 7 - setting bypass nested moderation to TRUE"+$originalDLConfiguration.msExchModerationFlags)
+
+            $functionModerationFlags=$TRUE
+
+            out-logfile ("The function moderation flags are = "+$functionModerationFlags)
+        }
+
+        if (($originalDLConfiguration.msExchModerationFlags -eq "0") -or ($originalDLConfiguration.msExchModerationFlags -eq "1")  )
+        {
+            out-logfile -string ("The moderation flags are 0 / 2 / 6 - setting bypass nested moderation to TRUE"+$originalDLConfiguration.msExchModerationFlags)
+
+            $functionSendModerationNotifications="Never"
+
+            out-logfile -string ("The function send moderations notifications is ="+$functionSendModerationNotifications)
+        }
+        elseif (($originalDLConfiguration.msExchModerationFlags -eq "2") -or ($originalDLConfiguration.msExchModerationFlags -eq "3")  )
+        {
+            out-logfile -string ("The moderation flags are 0 / 2 / 6 - setting bypass nested moderation to TRUE"+$originalDLConfiguration.msExchModerationFlags)
+
+            $functionSendModerationNotifications="Internal"
+
+            out-logfile -string ("The function send moderations notifications is ="+$functionSendModerationNotifications)
+
+        }
+        elseif (($originalDLConfiguration.msExchModerationFlags -eq "6") -or ($originalDLConfiguration.msExchModerationFlags -eq "7")  )
+        {
+            out-logfile -string ("The moderation flags are 0 / 2 / 6 - setting bypass nested moderation to TRUE"+$originalDLConfiguration.msExchModerationFlags)
+
+            $functionSendModerationNotifications="Always"
+
+            out-logfile -string ("The function send moderations notifications is ="+$functionSendModerationNotifications)
+        }
         
         try 
         {
             out-logfile -string "Setting the single value settings for the distribution group."
+
+            Set-O365DistributionGroup -Identity $originalDLConfiguration.mailNickName -BypassNestedModerationEnabled $functionModerationFlags -MemberJoinRestriction $originalDLConfiguration.msExchGroupJoinRestriction -MemberDepartRestriction $functionMemberDepartRestriction -ReportToManagerEnabled $originalDLConfiguration.reportToOwner -ReportToOriginatorEnabled $originalDLConfiguration.reportToOriginator -SendOofMessageToOriginatorEnabled $originalDLConfiguration.oofReplyToOriginator -Alias $originalDLConfiguration.mailNickName -CustomAttribute1 $originalDLConfiguration.extensionAttribute1 -CustomAttribute10 $originalDLConfiguration.extensionAttribute10 -CustomAttribute11 $originalDLConfiguration.extensionAttribute11 -CustomAttribute12 $originalDLConfiguration.extensionAttribute12 -CustomAttribute13 $originalDLConfiguration.extensionAttribute13 -CustomAttribute14 $originalDLConfiguration.extensionAttribute14 -CustomAttribute15 $originalDLConfiguration.extensionAttribute15 -CustomAttribute2 $originalDLConfiguration.extensionAttribute2 -CustomAttribute3 $originalDLConfiguration.extensionAttribute3 -CustomAttribute4 $originalDLConfiguration.extensionAttribute4 -CustomAttribute5 $originalDLConfiguration.extensionAttribute5 -CustomAttribute6 $originalDLConfiguration.extensionAttribute6 -CustomAttribute7 $originalDLConfiguration.extensionAttribute7 -CustomAttribute8 $originalDLConfiguration.extensionAttribute8 -CustomAttribute9 $originalDLConfiguration.extensionAttribute9 -ExtensionCustomAttribute1 $originalDLConfiguration.msExtensionCustomAttribute1 -ExtensionCustomAttribute2 $originalDLConfiguration.msExtensionCustomAttribute2 -ExtensionCustomAttribute3 $originalDLConfiguration.msExtensionCustomAttribute3 -ExtensionCustomAttribute4 $originalDLConfiguration.msExtensionCustomAttribute4 -ExtensionCustomAttribute5 $originalDLConfiguration.msExtensionCustomAttribute5 -DisplayName $originalDLConfiguration.DisplayName -HiddenFromAddressListsEnabled $originalDLConfiguration.msExchHideFromAddressLists -ModerationEnabled $originalDLConfiguration.msExchEnableModeration -RequireSenderAuthenticationEnabled $originalDLConfiguration.msExchRequireAuthToSendTo -SimpleDisplayName $originalDLConfiguration.DisplayNamePrintable -SendModerationNotifications $functionSendModerationNotification -WindowsEmailAddress $originalDLConfiguration.mail -MailTipTranslations $originalDLConfiguration.msExchSenderHintTranslations -Name $originalDLConfiguration.cn
         }
         catch 
         {
