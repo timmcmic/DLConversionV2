d9e76b1442eddc789047ffe59b9c4142eaac2835
TIMMCMIC <timmcmic@microsoft.com>
2021-03-25 13:53:58 -0400

    Test

--- Out-LogFile.ps1
+++ Out-LogFile.ps1
@@ -72,7 +72,7 @@
         }
         else 
         {
-            Write-host ($logstring | select-object *).tostring()
+            Write-host $string
         }
 
         #If the output to the log is terminating exception - throw the same string.
