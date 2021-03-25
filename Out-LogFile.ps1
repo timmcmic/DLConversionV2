ddf366ce7021918b8bb5a5d0d4400b3e29252957
TIMMCMIC <timmcmic@microsoft.com>
2021-03-25 10:11:59 -0400

    Updating

--- Out-LogFile.ps1
+++ Out-LogFile.ps1
@@ -73,7 +73,7 @@
         else 
         {
             out-logfile -string "I made it here."
-            Write-Output $logstring 6>&1
+            Write-Verbose $logstring
         }
 
         #If the output to the log is terminating exception - throw the same string.
