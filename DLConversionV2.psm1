153e923ce5617cf88da61ee364292d01fc8d5d45
TIMMCMIC <timmcmic@microsoft.com>
2021-03-28 14:16:28 -0400

    Trying again for this search name

--- DLConversionV2.psm1
+++ DLConversionV2.psm1
@@ -1752,8 +1752,9 @@ Function Start-DistributionListMigration
 
     try {
         $tempOU=$originalDLConfiguration.distinguishedName.substring($originalDLConfiguration.distinguishedName.indexof("OU"))
-        $tempName=$originalDLConfiguration.cn.replace(' ','')
-        $tempName=$tempNameArray+"-MigratedByScript"
+        $tempName=$originalDLConfiguration.cn
+        $tempName=$tempname.replace(' ','')
+        $tempName=$tempName+"-MigratedByScript"
         $tempDN=$tempName+","+$tempOU
         $routingContactConfiguration = Get-ADObjectConfiguration -dn $tempDN -globalCatalogServer $globalCatalogWithPort -parameterSet $dlPropertySet -errorAction STOP -adCredential $activeDirectoryCredential 
     }
