db41528f5f0ca0cd7c1bc75f4914fcf74d670bd6
TIMMCMIC <timmcmic@microsoft.com>
2021-03-30 11:39:00 -0400

    Commiting new function to remove PSSessions

--- Disable-OriginalDL.ps1
+++ Disable-OriginalDL.ps1
@@ -1,27 +1,11 @@
 <#
     .SYNOPSIS
 
-    This function disabled the on premies distribution list - removing it from azure ad and exchange online.
+    This function disables all open powershell sessions.
 
     .DESCRIPTION
 
-    This function disabled the on premies distribution list - removing it from azure ad and exchange online.
-
-    .PARAMETER parameterSet
-
-    These are the parameters that will be manually cleared from the object in AD mode.
-
-    .PARAMETER DN
-
-    The DN of the group to remove.
-
-    .PARAMETER GlobalCatalog
-
-    The global catalog server the operation should be performed on.
-
-    .PARAMETER UseExchange
-
-    If set to true disablement will occur using the exchange on premises powershell commands.
+    This function disables all open powershell sessions.
 
     .OUTPUTS
 
@@ -29,96 +13,34 @@
 
     .EXAMPLE
 
-    Get-ADObjectConfiguration -powershellsessionname NAME -groupSMTPAddress Address
+    disable-allPowerShellSessions
 
     #>
-    Function Disable-OriginalDL
+    Function disable-allPowerShellSessions
      {
-        [cmdletbinding()]
-
-        Param
-        (
-            [Parameter(Mandatory = $true,ParameterSetName = "AD")]
-            [Parameter(Mandatory = $true,ParameterSetName = "Exchange")]
-            $originalDLConfiguration,
-            [Parameter(Mandatory = $true,ParameterSetName = "AD")]
-            [Parameter(Mandatory = $true,ParameterSetName = "Exchange")]
-            [string]$globalCatalogServer,
-            [Parameter(Mandatory = $false,ParameterSetName = "AD")]
-            [array]$parameterSet="None",
-            [Parameter(Mandatory = $false,ParameterSetName="Exchange")]
-            [boolean]$useOnPremsiesExchange=$FALSE,
-            [Parameter(Mandatory = $true)]
-            $adCredential
-        )
-
-        #Declare function variables.
-
-        $functionDLConfiguration=$NULL #Holds the return information for the group query.
-        [string]$functionCustomAttribute1="MigratedByScript"
-        [string]$functionCustomAttribute2=$originalDLConfiguration.mail
-
-
-
-        #Start function processing.
 
         Out-LogFile -string "********************************************************************************"
-        Out-LogFile -string "BEGIN Disable-OriginalDLConfiguration"
+        Out-LogFile -string "BEGIN disable-allPowerShellSessions"
         Out-LogFile -string "********************************************************************************"
 
-        #Log the parameters and variables for the function.
+        out-logFile -string "Disconnecting Exchange Online Session"
 
-        Out-LogFile -string ("OriginalDLConfiguration = "+$originalDLConfiguration)
-        Out-LogFile -string ("GlobalCatalogServer = "+$globalCatalogServer)
-        out-logfile -string ("Use Exchange On Premises ="+$useOnPremsiesExchange)
-        out-logfile -string ("DN of object to modify / disable "+$originalDLConfiguration.distinguishedName)
-
-        OUt-LogFile -string ("Parameter Set:")
-        
-        foreach ($parameterIncluded in $parameterSet)
-        {
-            Out-Logfile -string $parameterIncluded
+        try{
+            Disconnect-ExchangeOnline -confirm:$FALSE
+        }
+        catch{
+            out-logFile -string $_ -isError:$TRUE
         }
 
-        out-logfile -string ("Disalbed DL Custom Attribute 1 = "+$functionCustomAttribute1)
-        out-logfile -string ("Disabled DL Custom Attribute 2 = "+$functionCustomAttribute2)
+        out-logfile -string "Remove all other PSSessions"
 
-        #Get the group using LDAP / AD providers.
-        
-        try 
-        {
-            Out-LogFile -string "Determine if exchange should be utilized to clear the DL."
-
-            if ($useOnPremsiesExchange -eq $FALSE)
-            {
-                Out-LogFile -string "Using AD providers to clear the given attributes"
-
-                set-adgroup -identity $originalDLConfiguration.distinguishedName -server $globalCatalogServer -clear $parameterSet -credential $adCredential
-            }
-
-            elseif ($useOnPremsiesExchange -eq $TRUE)
-            {
-                out-logfile -string "Using Exchange providers to clear the distribution list."
-
-                disable-distributionGroup -identity $originalDLConfiguration.distinguishedName -domainController $globalCatalogServer -confirm:$false -verbose
-            }
+        try{
+            Get-PSSession | remove-psssession
         }
-        catch 
-        {
-            Out-LogFile -string $_ -isError:$TRUE
+        catch{
+            out-logfile -string $_ -isError:TRUE
         }
 
-        #Now that the DL is disabled - use this oppurtunity to write the custom attributes to show it's been migrated.
-
-        out-logfile -string "The group has been migrated and is retained - set custom attributes with original information for other migration dependencies."
-        
-        try {
-            set-adgroup -identity $originalDLConfiguration.distinguishedName -add @{extensionAttribute1=$functionCustomAttribute1;extensionAttribute2=$functionCustomAttribute2} -server $globalCatalogServer
-        }
-        catch {
-            out-logfile -string $_ -isError:$TRUE
-        }
-
-        Out-LogFile -string "END Disable-OriginalDLConfiguration"
+        Out-LogFile -string "END disable-allPowerShellSessions"
         Out-LogFile -string "********************************************************************************"
     }
