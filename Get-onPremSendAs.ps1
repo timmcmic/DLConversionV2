8508401389d94c1619d78bff0aaab075cda78ebe
TIMMCMIC <timmcmic@microsoft.com>
2021-04-12 20:41:16 -0400

    Fixing argument call

--- Get-onPremSendAs.ps1
+++ Get-onPremSendAs.ps1
@@ -40,7 +40,7 @@
         Out-LogFile -string "BEGIN Get-onPremSendAs"
         Out-LogFile -string "********************************************************************************"
 
-        out-logfile -string ("DL query name = "+$functionDLQueryName)
+        out-logfile -string ("DL query name = "+$functionQueryName)
 
         #Start function processing.
 
@@ -53,7 +53,7 @@
         foreach ($recipient in $functionRecipients)
         {
             write-host ("Processing recipient = "+$recipient.identity)
-            $functionSendAsRights+= invoke-command {Get-ADPermission | Where-Object {($_.ExtendedRights -like "*send-as*") -and -not ($_.User -like "nt authority\self") -and ($_.isInherited -eq $false) -and $_.user -like $args} -ArgumentList $functionQueryName}
+            $functionSendAsRights+= invoke-command {Get-ADPermission | Where-Object {($_.ExtendedRights -like "*send-as*") -and -not ($_.User -like "nt authority\self") -and ($_.isInherited -eq $false) -and $_.user -like $args} }-ArgumentList $functionQueryName
         }
 
         out-logfile -string $functionSendAsRights
