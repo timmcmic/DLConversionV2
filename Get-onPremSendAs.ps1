8552b43fbbd53199ad43b15b9b0161af5eb23f05
TIMMCMIC <timmcmic@microsoft.com>
2021-04-12 20:57:06 -0400

    Updated

--- Get-onPremSendAs.ps1
+++ Get-onPremSendAs.ps1
@@ -53,7 +53,7 @@
         foreach ($recipient in $functionRecipients)
         {
             write-host ("Processing recipient = "+$recipient.identity)
-            $functionSendAsRights+= invoke-command {Get-ADPermission -identity $args[0] | Where-Object {($_.ExtendedRights -like "*send-as*") -and -not ($_.User -like "nt authority\self") -and ($_.isInherited -eq $false) -and ($_.user -like $args[1])}}-ArgumentList $recipient.identity,$functionQueryName
+            $functionSendAsRights+= invoke-command {$blockName=$args[1];Get-ADPermission -identity $args[0] | Where-Object {($_.ExtendedRights -like "*send-as*") -and -not ($_.User -like "nt authority\self") -and ($_.isInherited -eq $false) -and ($_.user -like $blockName)}}-ArgumentList $recipient.identity,$functionQueryName
         } 
 
         out-logfile -string $functionSendAsRights
