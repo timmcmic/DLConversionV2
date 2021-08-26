07453def3d72113730661fe425dad6a0c65bd9fd
TIMMCMIC <timmcmic@microsoft.com>
2021-08-26 05:32:43 -0400

    Progress status update

--- start-collectOffice365MailboxFolders.ps1
+++ start-collectOffice365MailboxFolders.ps1
@@ -288,7 +288,7 @@ function start-collectOffice365MailboxFolders
 
             $MbxNumber++
 
-            $progressString = "Mailbox Name: "+$mailbox.primarySMTPAddress+" Mailbox Number: "+$mailboxCounter
+            $progressString = "Mailbox Name: "+$mailbox.primarySMTPAddress+" Mailbox Number: "+$mailboxCounter+" of "+$totalMailboxes
 
             Write-Progress -Activity "Processing mailbox" -Status $progressString -PercentComplete $PercentComplete -Id 1
 
