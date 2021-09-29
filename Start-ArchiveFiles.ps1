e310d44710209a95bb1cf0879d4a2fcafea1e4d6
TIMMCMIC <timmcmic@microsoft.com>
2021-09-28 20:54:59 -0400

    Updating to include a missing sleep

--- Start-ArchiveFiles.ps1
+++ Start-ArchiveFiles.ps1
@@ -83,6 +83,7 @@
                     }
                     else 
                     {
+                        start-sleep -s 5
                         $doCounter=$doCounter+1
                     }
                 }
