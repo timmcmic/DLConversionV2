911b048b97e3b51257a169969b00b4f46328a03c
TIMMCMIC <timmcmic@microsoft.com>
2022-02-08 20:06:42 -0500

    Fixing function to return name

--- get-distinguishedName.ps1
+++ get-distinguishedName.ps1
@@ -8,6 +8,8 @@ function  Get-DistinguishedName {
         [string]$CanonicalName
     )
 
+    [string]$returnDN = ""
+
     Out-LogFile -string "********************************************************************************"
     Out-LogFile -string "BEGIN GET-DistinguishedName"
     Out-LogFile -string "********************************************************************************"
@@ -22,14 +24,14 @@ function  Get-DistinguishedName {
         $output += $arr[0] -replace '^.*$', 'CN=$0'
         $output += ($arr | select -Skip 1 | select -SkipLast 1) -replace '^.*$', 'OU=$0'
         $output += ($arr | ? { $_ -like '*.*' }) -split '\.' -replace '^.*$', 'DC=$0'
-        $output -join ','
+        $returnDN = $output -join ','
     }
 
-    out-logfile -string ("Converted canonical name: "+$output)
+    out-logfile -string ("Converted canonical name: "+$returnDN)
     
     Out-LogFile -string "********************************************************************************"
     Out-LogFile -string "END Get-DistinguishedName"
     Out-LogFile -string "********************************************************************************"
 
-    return $output    
+    return $returnDN   
 }
