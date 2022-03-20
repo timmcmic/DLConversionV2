bca34f10b60cf178d60b7cbc15578e25fedcb2a5
timmcmic@microsoft.com <timmcmic@microsoft.com>
2022-03-20 14:46:50 -0400

    Fixed filter on permissions

--- Get-O365DLMailboxFolderPermissions.ps1
+++ Get-O365DLMailboxFolderPermissions.ps1
@@ -82,7 +82,7 @@
 
             out-logfile -string ("Post count: "+$collectedData.count)
 
-            $functionFolderAccess = $collectedData | where {$_.user.userprincipalname.contains($groupSMTPAddress)}
+            $functionFolderAccess = $collectedData | where {$_.user.contains($groupSMTPAddress)}
         }
 
         write-progress -activity "Processing Recipient" -completed
