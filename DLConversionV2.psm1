72865124618e8a8280246a3638f5b6dfce4f6115
TIMMCMIC <timmcmic@microsoft.com>
2022-09-11 15:43:22 -0400

    Not sure why new log file was not working in this instance.

--- DLConversionV2.psm1
+++ DLConversionV2.psm1
@@ -236,28 +236,6 @@ Function Start-DistributionListMigration
         $logFolderPath=$logFolderPath+$threadFolder[$global:threadNumber]
     }
 
-    #Log start of DL migration to the log file.
-
-    new-LogFile -groupSMTPAddress $groupSMTPAddress.trim() -logFolderPath $logFolderPath
-
-    #Output all parameters bound or unbound and their associated values.
-
-    out-logfile -string "Output bound parameters..."
-
-    foreach ($paramName in $MyInvocation.MyCommand.Parameters.Keys)
-    {
-        $bound = $PSBoundParameters.ContainsKey($paramName)
-
-        $parameterObject = New-Object PSObject -Property @{
-            ParameterName = $paramName
-            ParameterValue = if ($bound) { $PSBoundParameters[$paramName] }
-                             else { Get-Variable -Scope Local -ErrorAction Ignore -ValueOnly $paramName }
-            Bound = $bound
-          }
-
-          out-logfile -string $parameterObject
-    }
-
     #For mailbox folder permissions set these to false.
     #Supported methods for gathering folder permissions require use of the pre-collection.
     #Precolletion automatically sets these to true.  These were origianlly added to support doing it at runtime - but its too slow.
@@ -546,6 +524,28 @@ Function Start-DistributionListMigration
             remove-statusFiles -fullCleanup:$TRUE
         }
     }
+
+    #Log start of DL migration to the log file.
+
+    new-LogFile -groupSMTPAddress $groupSMTPAddress.trim() -logFolderPath $logFolderPath
+
+        #Output all parameters bound or unbound and their associated values.
+
+        out-logfile -string "Output bound parameters..."
+
+        foreach ($paramName in $MyInvocation.MyCommand.Parameters.Keys)
+        {
+            $bound = $PSBoundParameters.ContainsKey($paramName)
+    
+            $parameterObject = New-Object PSObject -Property @{
+                ParameterName = $paramName
+                ParameterValue = if ($bound) { $PSBoundParameters[$paramName] }
+                                 else { Get-Variable -Scope Local -ErrorAction Ignore -ValueOnly $paramName }
+                Bound = $bound
+              }
+    
+              out-logfile -string $parameterObject
+        }
 
     Out-LogFile -string "================================================================================"
     Out-LogFile -string "BEGIN START-DISTRIBUTIONLISTMIGRATION"
