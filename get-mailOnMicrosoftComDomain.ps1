f23209323a67ed471443ffa225caa22d1ed15002
TIMMCMIC <timmcmic@microsoft.com>
2022-09-11 15:34:14 -0400

    Removing param output code

--- get-mailOnMicrosoftComDomain.ps1
+++ get-mailOnMicrosoftComDomain.ps1
@@ -15,24 +15,6 @@
     #>
     Function Get-MailOnMicrosoftComDomain
      {
-        [cmdletbinding()]
-
-        out-logfile -string "Output bound parameters..."
-
-        foreach ($paramName in $MyInvocation.MyCommand.Parameters.Keys)
-        {
-            $bound = $PSBoundParameters.ContainsKey($paramName)
-
-            $parameterObject = New-Object PSObject -Property @{
-                ParameterName = $paramName
-                ParameterValue = if ($bound) { $PSBoundParameters[$paramName] }
-                                else { Get-Variable -Scope Local -ErrorAction Ignore -ValueOnly $paramName }
-                Bound = $bound
-            }
-
-            out-logfile -string $parameterObject
-        }
-
         #Define variables that will be utilzed in the function.
 
         [string]$functionDomainName = ""
