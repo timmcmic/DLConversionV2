e4ff4621ef072e3b9fdbb9fb588b59655bf0c68a
TIMMCMIC <timmcmic@microsoft.com>
2022-09-11 15:46:12 -0400

    Updatin

--- DLConversionV2.psm1
+++ DLConversionV2.psm1
@@ -529,23 +529,23 @@ Function Start-DistributionListMigration
 
     new-LogFile -groupSMTPAddress $groupSMTPAddress.trim() -logFolderPath $logFolderPath
 
-        #Output all parameters bound or unbound and their associated values.
+    #Output all parameters bound or unbound and their associated values.
 
-        out-logfile -string "Output bound parameters..."
+    out-logfile -string "Output bound parameters..."
 
-        foreach ($paramName in $MyInvocation.MyCommand.Parameters.Keys)
-        {
-            $bound = $PSBoundParameters.ContainsKey($paramName)
-    
-            $parameterObject = New-Object PSObject -Property @{
-                ParameterName = $paramName
-                ParameterValue = if ($bound) { $PSBoundParameters[$paramName] }
-                                 else { Get-Variable -Scope Local -ErrorAction Ignore -ValueOnly $paramName }
-                Bound = $bound
-              }
-    
-              out-logfile -string $parameterObject
-        }
+    foreach ($paramName in $MyInvocation.MyCommand.Parameters.Keys)
+    {
+        $bound = $PSBoundParameters.ContainsKey($paramName)
+
+        $parameterObject = New-Object PSObject -Property @{
+            ParameterName = $paramName
+            ParameterValue = if ($bound) { $PSBoundParameters[$paramName] }
+                                else { Get-Variable -Scope Local -ErrorAction Ignore -ValueOnly $paramName }
+            Bound = $bound
+            }
+
+            out-logfile -string $parameterObject
+    }
 
     Out-LogFile -string "================================================================================"
     Out-LogFile -string "BEGIN START-DISTRIBUTIONLISTMIGRATION"
