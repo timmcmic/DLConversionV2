f0514d74d956fbdfa608c51d389858efa06214c8
TIMMCMIC <timmcmic@microsoft.com>
2022-09-11 23:59:01 -0400

    Fixing spelling

--- DLConversionV2.psm1
+++ DLConversionV2.psm1
@@ -531,9 +531,7 @@ Function Start-DistributionListMigration
 
     #Output all parameters bound or unbound and their associated values.
 
-    write-funcionParameters -keyArray $MyInvocation.MyCommand.Parameters.Keys -parameterArray $PSBoundParameters -variableArray (Get-Variable -Scope Local -ErrorAction Ignore)
-
-    out-logfile -string $parameterOutput
+    write-functionParameters -keyArray $MyInvocation.MyCommand.Parameters.Keys -parameterArray $PSBoundParameters -variableArray (Get-Variable -Scope Local -ErrorAction Ignore)
 
     Out-LogFile -string "================================================================================"
     Out-LogFile -string "BEGIN START-DISTRIBUTIONLISTMIGRATION"
