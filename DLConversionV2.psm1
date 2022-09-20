0e12882f161c540401d898a8ec2daefe3f3a901c
timmcmic <42809081+timmcmic@users.noreply.github.com>
2022-09-20 10:00:34 -0400

    Fixing error details (#85)
    * Fixing the missing isTestErrorDetail so that the message appears in the summary table.
    
    * Fixing invalid parameter referencesin start-multiple
    
    * Fixing thread folder declaration
    
    * Moving declarations up so they're in the right place

--- DLConversionV2.psm1
+++ DLConversionV2.psm1
@@ -408,6 +408,23 @@ Function Start-DistributionListMigration
     $windowTitle = ("Start-DistributionListMigration "+$groupSMTPAddress)
     $host.ui.RawUI.WindowTitle = $windowTitle
 
+    #Define global variables.
+
+    $global:threadNumber=$threadNumberAssigned
+    $global:logFile=$NULL #This is the global variable for the calculated log file name
+    [string]$global:staticFolderName="\DLMigration\"
+    [string]$global:staticAuditFolderName="\AuditData\"
+    [string]$global:importFile=$logFolderPath+$global:staticAuditFolderName
+
+    #Define variables for import data - used for importing data into pre-collect.
+
+    [array]$importData=@() #Empty array for the import data.
+    [string]$importFilePath=$NULL #Import file path where the XML data is located to import (calculated later)
+
+    #Define the sub folders for multi-threading.
+
+    [array]$threadFolder="\Thread0","\Thread1","\Thread2","\Thread3","\Thread4","\Thread5","\Thread6","\Thread7","\Thread8","\Thread9","\Thread10"
+
     #If multi threaded - the log directory needs to be created for each thread.
     #Create the log folder path for status before changing the log folder path.
 
@@ -444,19 +461,6 @@ Function Start-DistributionListMigration
             exit
         }
     }
-
-    #Define global variables.
-
-    $global:threadNumber=$threadNumberAssigned
-    $global:logFile=$NULL #This is the global variable for the calculated log file name
-    [string]$global:staticFolderName="\DLMigration\"
-    [string]$global:staticAuditFolderName="\AuditData\"
-    [string]$global:importFile=$logFolderPath+$global:staticAuditFolderName
-
-    #Define variables for import data - used for importing data into pre-collect.
-
-    [array]$importData=@() #Empty array for the import data.
-    [string]$importFilePath=$NULL #Import file path where the XML data is located to import (calculated later)
 
     #Define variables utilized in the core function that are not defined by parameters.
 
@@ -663,10 +667,6 @@ Function Start-DistributionListMigration
 
 
     [int]$forLoopTrigger=1000
-
-    #Define the sub folders for multi-threading.
-
-    [array]$threadFolder="\Thread0","\Thread1","\Thread2","\Thread3","\Thread4","\Thread5","\Thread6","\Thread7","\Thread8","\Thread9","\Thread10"
 
     #Define the status directory.
 
@@ -4376,6 +4376,7 @@ Function Start-DistributionListMigration
                 }
                 catch{
                     out-logfile -string $_
+                    $isTestErrorDetail = $_
                     $isTestError="Yes"
                 }
 
@@ -4437,6 +4438,7 @@ Function Start-DistributionListMigration
                 }
                 catch{
                     out-logfile -string $_
+                    $isTestErrorDetail = $_
                     $isTestError="Yes"
                 }
 
@@ -4498,6 +4500,7 @@ Function Start-DistributionListMigration
                 }
                 catch{
                     out-logfile -string $_
+                    $isTestErrorDetail = $_
                     $isTestError="Yes"
                 }
 
@@ -4559,6 +4562,7 @@ Function Start-DistributionListMigration
                 }
                 catch{
                     out-logfile -string $_
+                    $isTestErrorDetail = $_
                     $isTestError="Yes"
                 }
 
@@ -4620,6 +4624,7 @@ Function Start-DistributionListMigration
                 }
                 catch{
                     out-logfile -string $_
+                    $isTestErrorDetail = $_
                     $isTestError="Yes"
                 }
 
@@ -4681,6 +4686,7 @@ Function Start-DistributionListMigration
                 }
                 catch{
                     out-logfile -string $_
+                    $isTestErrorDetail = $_
                     $isTestError="Yes"
                 }
 
@@ -4754,6 +4760,7 @@ Function Start-DistributionListMigration
                     }
                     catch{
                         out-logfile -string $_
+                        $isTestErrorDetail = $_
                         $isTestError="Yes"
                     }
 
@@ -4826,6 +4833,7 @@ Function Start-DistributionListMigration
             }
             catch{
                 out-logfile -string $_
+                $isTestErrorDetail = $_
                 $isTestError="Yes"
             }
 
@@ -5265,6 +5273,7 @@ Function Start-DistributionListMigration
         }
         catch{
             out-logfile -string $_
+            $isTestErrorDetail = $_
             $isTestError="Yes"
         }
 
@@ -5293,6 +5302,8 @@ Function Start-DistributionListMigration
                 if($loopCounter -gt 10)
                 {
                     out-logfile -string "Unable to obtain the routing group after multiple tries."
+
+                    $isTestErrorDetail = $_
 
                     $isErrorObject = new-Object psObject -property @{
                         errorMessage = "Unable to obtain the routing group after multiple tries."
@@ -5347,6 +5358,8 @@ Function Start-DistributionListMigration
 
     if ($isTestError -eq "Yes")
     {
+        $isTestErrorDetail = $_
+
         $isErrorObject = new-Object psObject -property @{
             errorMessage = "Unable to trigger upgrade to Office 365 Unified / Modern group.  Administrator may need to manually perform the operation."
             erroMessageDetail = $isTestErrorDetail
@@ -5374,6 +5387,8 @@ Function Start-DistributionListMigration
 
     if ($isTestError -eq "Yes")
     {
+        $isTestErrorDetail = $_
+
         $isErrorObject = new-Object psObject -property @{
             errorMessage = "Uanble to remove the on premises group at request of administrator.  Group may need to be manually removed."
             erroMessageDetail = $isTestErrorDetail
