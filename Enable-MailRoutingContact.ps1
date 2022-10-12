8cf7c56e6295bee4ae0cba6e184874c272122b18
Timothy McMichael <timmcmic@microsoft.com>
2022-10-11 21:15:14 -0400

    Updating

--- Enable-MailRoutingContact.ps1
+++ Enable-MailRoutingContact.ps1
@@ -47,6 +47,7 @@
         #Declare function variables.
 
         $functionGroup=$NULL
+        $functionOutputGroup=$NULL
         $functionRemoteRoutingAddress=$NULL
 
         #Start function processing.
@@ -62,7 +63,7 @@
         try{
             out-logfile -string "Updating the mail contact..."
 
-            update-recipient -identity $routingContactConfig.mailNickName -domainController $globalCatalogServer -errorAction STOP
+            update-recipient -identity $routingContactConfig.distinguishsedName -domainController $globalCatalogServer -errorAction STOP
         }
         catch{
             out-logfile -string $_
@@ -75,12 +76,23 @@
         try{
             out-logfile -string "Gathering the mail contact configuration..."
 
-            $functionGroup=get-mailContact -identity $routingContactConfig.mailNickName -domainController $globalCatalogServer -errorAction STOP
+            $functionGroup=get-mailContact -identity $routingContactConfig.distinguishedName -domainController $globalCatalogServer -errorAction STOP
         }
         catch{
             out-logfile -string $_
             $isTestError="Yes"
             return $isTestError
+        }
+
+        #Write the updated recipient out.
+
+        try
+        {
+            out-xmlFile -itemToExport $functionGroup -itemNameTOExport (($routingXMLFile)+"-UpdateRecipient")
+        }
+        catch {
+            out-logfile -string "Unable to output to XML file."
+            out-logfile -string $_ -isError:$TRUE
         }
 
         #The mail contact may need upgrade to the "latest version."
@@ -88,12 +100,36 @@
         try{
             out-logfile -string "Forcing upgrade to contact - necessary in order to provision."
 
-            set-mailcontact -identity $functionGroup.alias -domainController $globalCatalogServer -ForceUpgrade
+            set-mailcontact -identity $functionGroup.distinguishedName -domainController $globalCatalogServer -ForceUpgrade
         }
         catch{
             out-logfile -string $_
             $isTestError="Yes"
             return $isTestError
+        }
+
+        #Obtain the mail contact configuration into a temporary variable now that it's a full recipient.
+
+        try{
+            out-logfile -string "Gathering the mail contact configuration..."
+
+            $functionOutputGroup=get-mailContact -identity $routingContactConfig.distinguishedName -domainController $globalCatalogServer -errorAction STOP
+        }
+        catch{
+            out-logfile -string $_
+            $isTestError="Yes"
+            return $isTestError
+        }
+
+        #Write the updated recipient out.
+
+        try
+        {
+            out-xmlFile -itemToExport $functionOutputGroup -itemNameTOExport (($routingXMLFile)+"-UpgradedRecipient")
+        }
+        catch {
+            out-logfile -string "Unable to output to XML file."
+            out-logfile -string $_ -isError:$TRUE
         }
 
         #The mail contact may need upgrade to the "latest version."
@@ -101,7 +137,7 @@
         try{
             out-logfile -string "Setting email address policy enabled to $FALSE - stop further automatic email addressing."
 
-            set-mailcontact -identity $functionGroup.alias -EmailAddressPolicyEnabled:$FALSE -domainController $globalCatalogServer -forceUpgrade -confirm:$FALSE
+            set-mailcontact -identity $functionGroup.distinguishedName -EmailAddressPolicyEnabled:$FALSE -domainController $globalCatalogServer -forceUpgrade -confirm:$FALSE
         }
         catch{
             out-logfile -string $_
@@ -109,27 +145,101 @@
             return $isTestError
         }
 
+        #Obtain the mail contact configuration into a temporary variable now that it's a full recipient.
+
+        try{
+            out-logfile -string "Gathering the mail contact configuration..."
+
+            $functionOutputGroup=get-mailContact -identity $routingContactConfig.distinguishedName -domainController $globalCatalogServer -errorAction STOP
+        }
+        catch{
+            out-logfile -string $_
+            $isTestError="Yes"
+            return $isTestError
+        }
+
+        #Write the updated recipient out.
+
+        try
+        {
+            out-xmlFile -itemToExport $functionOutputGroup -itemNameTOExport (($routingXMLFile)+"-DisableEmailPolicy")
+        }
+        catch {
+            out-logfile -string "Unable to output to XML file."
+            out-logfile -string $_ -isError:$TRUE
+        }
+
         try{
             out-logfile -string "Removing the remote routing address..."
 
-            set-mailContact -identity $routingContactConfig.mailNickName -primarySMTPAddress $routingContactConfig.mail -domainController $globalCatalogServer -forceUpgrade -confirm:$FALSE -errorAction STOP
+            set-mailContact -identity $routingContactConfig.distinguishedName -primarySMTPAddress $routingContactConfig.mail -domainController $globalCatalogServer -forceUpgrade -confirm:$FALSE -errorAction STOP
         }
         catch{
             out-logfile -string $_ -isError:$TRUE
         }
+
+        #Obtain the mail contact configuration into a temporary variable now that it's a full recipient.
+
+        try{
+            out-logfile -string "Gathering the mail contact configuration..."
+
+            $functionOutputGroup=get-mailContact -identity $routingContactConfig.distinguishedName -domainController $globalCatalogServer -errorAction STOP
+        }
+        catch{
+            out-logfile -string $_
+            $isTestError="Yes"
+            return $isTestError
+        }
+
+        #Write the updated recipient out.
+
+        try
+        {
+            out-xmlFile -itemToExport $functionOutputGroup -itemNameTOExport (($routingXMLFile)+"-MailUpdate")
+        }
+        catch {
+            out-logfile -string "Unable to output to XML file."
+            out-logfile -string $_ -isError:$TRUE
+        }
+
 
         #Removee the target address from the list of proxy addresses so it does not collide with the migrated group.
 
         try{
             out-logfile -string "Removing the remote routing address..."
 
-            set-mailContact -identity $routingContactConfig.mailNickName -emailaddresses @{remove=$routingContactConfig.targetAddress} -domainController $globalCatalogServer -forceUpgrade -confirm:$FALSE -errorAction STOP
+            set-mailContact -identity $routingContactConfig.distinguishedName -emailaddresses @{remove=$routingContactConfig.targetAddress} -domainController $globalCatalogServer -forceUpgrade -confirm:$FALSE -errorAction STOP
         }
         catch{
             out-logfile -string $_
             $isTestError="Yes"
             return $isTestError
         }
+
+        #Obtain the mail contact configuration into a temporary variable now that it's a full recipient.
+
+        try{
+            out-logfile -string "Gathering the mail contact configuration..."
+
+            $functionOutputGroup=get-mailContact -identity $routingContactConfig.distinguishedName -domainController $globalCatalogServer -errorAction STOP
+        }
+        catch{
+            out-logfile -string $_
+            $isTestError="Yes"
+            return $isTestError
+        }
+
+        #Write the updated recipient out.
+
+        try
+        {
+            out-xmlFile -itemToExport $functionOutputGroup -itemNameTOExport (($routingXMLFile)+"-RemoteTargetAddress")
+        }
+        catch {
+            out-logfile -string "Unable to output to XML file."
+            out-logfile -string $_ -isError:$TRUE
+        }
+
 
         Out-LogFile -string "END enable-mailRoutingContact"
         Out-LogFile -string "********************************************************************************"
