f983beaa4fa11a0eced6b5cb11e62ef7aab9357b
timmcmic <timmcmic@microsoft.com>
2022-11-10 12:10:38 -0500

    Updating

--- DLConversionV2.psm1
+++ DLConversionV2.psm1
@@ -5698,5 +5698,10 @@ Function Start-DistributionListMigration
 
     #Archive the files into a date time success folder.
 
+    out-logfile -string "*************************************************************"
+    out-logfile -string "Shameless plug...this script is developed in my spare time..."
+    out-logfile -string "If you are enjoying it - and it is working - consider letting my management know at dlconversionv2@service.microsoft.com"
+    out-logfile -string "*************************************************************"
+
     Start-ArchiveFiles -isSuccess:$TRUE -logFolderPath $logFolderPath
 }
