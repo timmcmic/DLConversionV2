8d446f72980ffb3d66cad8f94d3725b7161b8c3b
timmcmic <timmcmic@microsoft.com>
2022-12-13 12:04:19 -0500

    Updating

--- DLConversionV2.psm1
+++ DLConversionV2.psm1
@@ -2124,7 +2124,7 @@ Function Start-DistributionListMigration
 
     try 
     {
-        $exchangeSendAsSMTP=get-GroupSendAsPermissions -globalCatalog $corevariables.globalCatalogWithPort.value -dn $originalDLConfiguration.distinguishedName -adCredential $activeDirectoryCredential -adGlobalCatalogPowershellSessionName $coreVariables.ADGlobalCatalogPowershellSessionName.value -groupSTMPAddress $groupSMTPAddress
+        $exchangeSendAsSMTP=get-GroupSendAsPermissions -globalCatalog $corevariables.globalCatalogWithPort.value -dn $originalDLConfiguration.distinguishedName -adCredential $activeDirectoryCredential -adGlobalCatalogPowershellSessionName $coreVariables.ADGlobalCatalogPowershellSessionName.value -groupSMTPAddress $groupSMTPAddress
     }
     catch 
     {
