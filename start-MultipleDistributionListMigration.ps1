5a34bff9d11f07be70b719249ad6f0f8a9d215ac
timmcmic <timmcmic@microsoft.com>
2022-12-13 20:39:10 -0500

    Updating

--- start-MultipleDistributionListMigration.ps1
+++ start-MultipleDistributionListMigration.ps1
@@ -775,7 +775,7 @@ Function Start-MultipleDistributionListMigration
 
         foreach ($group in $nestedRetryGroups)
         {
-            for ($i = 0 ; $i -lt ($nestedRetryGroups -1 ) ; $i++)
+            for ($i = 0 ; $i -lt ($nestedRetryGroups.count-1 ) ; $i++)
             {
                 out-logfile -string ("Testing group: "+$group.primarySMTPAddressOrUPN+" Compared To: "+$nestedRetryGroups[$i].parentGroupSMTPAddress)
                 out-logfile -string ("Testing group: "+$group.parentGroupSMTPAddress+ "Compared To: "+$nestedGroups[$i].primarySMTPAddressorUPN)
