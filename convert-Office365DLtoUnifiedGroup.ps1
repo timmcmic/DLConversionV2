229dc4e2255bc0e9d0ec2cf04261a1aee2ae96e0
timmcmic <timmcmic@microsoft.com>
2023-01-17 12:38:54 -0500

    Updating

--- convert-Office365DLtoUnifiedGroup.ps1
+++ convert-Office365DLtoUnifiedGroup.ps1
@@ -1028,7 +1028,7 @@ Function Convert-Office365DLtoUnifiedGroup
     try {
         out-logfile -string "Obtianing the original Office 365 Group membership."
 
-        $office365DLMembership = get-o365DlMembership -groupSMTPAddres $groupSMTPAddress -errorAction STOP
+        $office365DLMembership = @(get-o365DlMembership -groupSMTPAddres $groupSMTPAddress -errorAction STOP)
 
     }
     catch {
