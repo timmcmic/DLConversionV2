2b0e34f44f4a53f2610564c7dc19f9e98622f48b
timmcmic <timmcmic@microsoft.com>
2023-01-22 12:27:17 -0500

    Update

--- start-Office365GroupMigration.ps1
+++ start-Office365GroupMigration.ps1
@@ -326,6 +326,8 @@ Function Start-Office365GroupMigration
         [boolean]$useCollectedFolderPermissionsOffice365=$FALSE,
         [Parameter(Mandatory = $false)]
         [boolean]$addManagersAsMembers = $false,
+        [Parameter(Mandatory = $false)]
+        [boolean]$overrideSecurityGroupCheck = $false,
         #Define parameters for multi-threaded operations
         [Parameter(Mandatory = $false)]
         [int]$threadNumberAssigned=0,
@@ -1208,6 +1210,21 @@ Function Start-Office365GroupMigration
     Out-XMLFile -itemToExport $originalDLConfiguration -itemNameToExport $xmlFiles.originalDLConfigurationADXML.value
 
     Out-LogFile -string "Determine if administrator desires to audit send as."
+
+    Out-logfile -string "Validating security group override."
+
+    if ((($originalDLConfiguration.groupType -eq "-2147483640") -or ($originalDLConfiguration.groupType -eq "-2147483646") -or ($originalDLConfiguration.groupType -eq "-2147483644")) -and ($overrideSecurityGroupCheck -eq $FALSE))
+    {
+        out-logfile -string "Group type on premises is security."
+        out-logfile -string "The administrator must specify -overrideSecurityGroupCheck to allow the migration to proceed."
+        out-logfile -string "Office 365 Groups are not security principals.  It is possible that permissions may be lost in Office 365 as a result of deleting and recreating the group during migration."
+
+        out-logfile -string "UNIFIED_GROUP_MIGRATION_GROUP_IS_SECURITY_EXCEPTION:  To perform an Office 365 Unified Group migration of a mail-enabled security group on premsies the administrator must use -overrideSecurityGroupCheck acknolwedging that permissions may be lost in Office 365 as a result of the migration." -isError:$TRUE
+    }
+    else 
+    {
+        out-logfile -string "Group is not security on premises therefore the administrator does not need to override and acknowledge potentially lost permissions."
+    }
 
     if ($retainSendAsOnPrem -eq $TRUE)
     {
