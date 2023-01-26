9c09228677f0acb225f63d95418206f20688a9fc
timmcmic <timmcmic@microsoft.com>
2023-01-26 07:12:36 -0500

    Updating

--- compare-recipientArrays.ps1
+++ compare-recipientArrays.ps1
@@ -19,6 +19,8 @@ function compare-recipientArrays
 
     out-logfile -string "Determine if we are comparing on premises and Azure <or> on premises and Exchange Online"
 
+    $onPremData.displayName
+
     if (($onPremData -ne $NULL) -and ($azureData -ne $NULL))
     {
         out-logfile -string "This is a comparison of on premises and Azure AD data."
