8494b5ae8b6c023699aa70bec9cdfa242970ecb6
timmcmic <timmcmic@microsoft.com>
2023-01-26 07:08:23 -0500

    Updating

--- compare-recipientArrays.ps1
+++ compare-recipientArrays.ps1
@@ -25,10 +25,6 @@ function compare-recipientArrays
 
         for ($i = ($onPremData.count-1) ; $i -ge 0 ; $i--)
         {
-            out-logfile $i
-            out-logfile $onPremData.Count
-            out-logfile $azureData.Count
-
             #Group members come in different flavors.
             #The first is a user type that is either mail enabled or not.  Any user object has this attribute - we search that first.
             #The second is a group type.  Regardless of group type the group SID is replicated into the original group sid in azure.  We search there next.
