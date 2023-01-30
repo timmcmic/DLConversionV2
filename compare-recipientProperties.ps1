4bf4f1b39bde7d93c616daedee438c448d43b2a9
timmcmic <timmcmic@microsoft.com>
2023-01-30 15:43:05 -0500

    Updating

--- compare-recipientProperties.ps1
+++ compare-recipientProperties.ps1
@@ -11,6 +11,8 @@ function compare-recipientProperties
 
     $functionReturnArray=@()
     $functionGroupType = $NULL
+    $functionModerationFlags = $NULL
+    $functionMemberJoinRestriction=$NULL
 
 
     Out-LogFile -string "********************************************************************************"
@@ -38,9 +40,9 @@ function compare-recipientProperties
             Attribute = "GroupType"
             OnPremisesValue = $functionGroupType
             AzureADValue = "N/A"
-            isPresentInAzure = "N/A"
+            isValidInAzure = "N/A"
             ExchangeOnlineValue = $office365Data.groupType            
-            isPresentInExchangeOnline = "True"
+            isValidInExchangeOnline = "True"
             IsValidMember = "TRUE"
             ErrorMessage = "N/A"
         }
@@ -57,9 +59,9 @@ function compare-recipientProperties
             Attribute = "GroupType"
             OnPremisesValue = $functionGroupType
             AzureADValue = "N/A"
-            isPresentInAzure = "N/A"
+            isValidInAzure = "N/A"
             ExchangeOnlineValue = $office365Data.groupType            
-            isPresentInExchangeOnline = "True"
+            isValidInExchangeOnline = "True"
             IsValidMember = "FALSE"
             ErrorMessage = "VALUE_ONPREMISES_NOT_EQUAL_OFFICE365_EXCEPTION"
         }
@@ -68,7 +70,123 @@ function compare-recipientProperties
 
         $functionReturnArray += $functionObject
     }
-    
+
+    out-logfile -string "Evaluate bypass nested moderation enabled."
+
+    if (($onPremData.msExchModerationFlags -eq "1") -or ($onPremData.msExchModerationFlags -eq "3") -or ($onPremData.msExchModerationFlags -eq "7") )
+    {
+        out-logfile -string $onPremData.msExchModerationFlags
+
+        $functionModerationFlags = $TRUE
+
+        out-logfile -string $functionModerationFlags
+    }
+    else 
+    {
+        out-logfile -string $onPremData.msExchModerationFlags
+
+        $functionModerationFlags = $FALSE
+
+        out-logfile -string $functionModerationFlags
+    }
+
+    if ($office365Data.BypassNestedModerationEnabled -eq $functionModerationFlags)
+    {
+        out-logfile -string "Bypass nested moderation enabled valid."
+
+        $functionObject = New-Object PSObject -Property @{
+            Attribute = "BypassNestedModerationEnabled"
+            OnPremisesValue = $functionModerationFlags
+            AzureADValue = "N/A"
+            isValidInAzure = "N/A"
+            ExchangeOnlineValue = $office365Data.bypassNestedModerationEnabled           
+            isValidInExchangeOnline = "True"
+            IsValidMember = "TRUE"
+            ErrorMessage = "N/A"
+        }
+
+        out-logfile -string $functionObject
+
+        $functionReturnArray += $functionObject
+    }
+    else 
+    {
+        out-logfile -string "Bypass nested moderation enabled invalid."
+
+        $functionObject = New-Object PSObject -Property @{
+            Attribute = "BypassNestedModerationEnabled"
+            OnPremisesValue = $functionModerationFlags
+            AzureADValue = "N/A"
+            isValidInAzure = "N/A"
+            ExchangeOnlineValue = $office365Data.bypassNestedModerationEnabled           
+            isValidInExchangeOnline = "False"
+            IsValidMember = "FALSE"
+            ErrorMessage = "N/A"
+        }
+
+        out-logfile -string $functionObject
+
+        $functionReturnArray += $functionObject
+    }
+
+    out-logfile -string "Evaluate member join restrictions."
+
+    if ($onPremData.msExchGroupJoinRestriction -eq $NULL)
+    {
+        out-logfile -string $onPremData.msExchGroupJoinRestriction
+
+        $functionMemberJoinRestriction="Closed"
+
+        out-logfile -string $functionMemberJoinRestriction
+    }
+    else 
+    {
+        out-logfile -string $onPremData.msExchGroupJoinRestriction
+
+        $functionMemberJoinRestriction = $onPremData.msExchGroupJoinRestriction
+
+        out-logfile -string $functionMemberJoinRestriction
+    }
+
+    if ($office365Data.MemberJoinRestriction -eq $functionMemberJoinRestriction)
+    {
+        out-logfile -string "Member join restriction is a valid value."
+
+        $functionObject = New-Object PSObject -Property @{
+            Attribute = "MemberJoinRestriction"
+            OnPremisesValue = $functionMemberJoinRestriction
+            AzureADValue = "N/A"
+            isValidInAzure = "N/A"
+            ExchangeOnlineValue = $office365Data.MemberJoinRestriction          
+            isValidInExchangeOnline = "True"
+            IsValidMember = "True"
+            ErrorMessage = "N/A"
+        }
+
+        out-logfile -string $functionObject
+
+        $functionReturnArray += $functionObject
+    }
+    else 
+    {
+        out-logfile -string "Member join restriction is a invalid value."
+
+        $functionObject = New-Object PSObject -Property @{
+            Attribute = "MemberJoinRestriction"
+            OnPremisesValue = $functionMemberJoinRestriction
+            AzureADValue = "N/A"
+            isValidInAzure = "N/A"
+            ExchangeOnlineValue = $office365Data.MemberJoinRestriction          
+            isValidInExchangeOnline = "False"
+            IsValidMember = "FALSE"
+            ErrorMessage = "N/A"
+        }
+
+        out-logfile -string $functionObject
+
+        $functionReturnArray += $functionObject    
+    }
+
     Out-LogFile -string "END compare-recipientProperties"
     Out-LogFile -string "********************************************************************************"
 
