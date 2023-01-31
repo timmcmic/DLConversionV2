87b83a54e35f8f26397da7474917d6c29701cd66
Timothy J. McMichael <timmcmic@microsoft.com>
2023-01-31 14:05:49 -0500

    Updating

--- compare-recipientArrays.ps1
+++ compare-recipientArrays.ps1
@@ -155,6 +155,12 @@ function compare-recipientArrays
 
                 out-logfile -string $functionPrimarySMTPAddress
             }
+            else 
+            {
+                out-logfile -string "Object does not have a proxy address - consider a synced security group?"
+
+                $functionPrimarySMTPAddress = "N/A"
+            }
 
             if ($azureData.objectID -contains $member.externalDirectoryObjectID)
             {
