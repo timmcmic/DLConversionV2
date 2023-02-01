4c281bc5716ebe7c2760f4ef3e5fffcb9acc32d2
timmcmic <timmcmic@microsoft.com>
2023-01-31 20:10:22 -0500

    Updating

--- get-DLHealthReport.ps1
+++ get-DLHealthReport.ps1
@@ -2968,9 +2968,9 @@ th {
         'MakeHiddenSection'=$true;
         'Properties'=   @{n='Attribute';e={$_.Attribute}},
                         @{n='OnPremisesValue';e={$_.OnPremisesValue}},
-                        @{n='IsValidInAzure';e={$_.isValidInAzure}},
-                        @{n='AzureADValue';e={$_.AzureADValue}}
-                        <#@{n='ExchangeOnlineValue';e={$_.ExchangeOnlineValue}}
+                        @{n='IsValidInAzure';e={$_.isValidInAzure}}
+                        <#@{n='AzureADValue';e={$_.AzureADValue}}
+                        @{n='ExchangeOnlineValue';e={$_.ExchangeOnlineValue}}
                         @{n='isValidInExchangeOnline';e={$_.isValidInExchangeOnline};css={if ($_.isValidInExchangeOnline -eq "False") { 'red' }}},
                         @{n='IsValidMember';e={$_.IsValidMember};css={if ($_.IsValidMember -eq "False"){ 'red' }}},
                         @{n='ErrorMessage';e={$_.ErrorMessage}}#>
