4a6fe10458ab60ab38b0c3312fef129273ffbf08
timmcmic <timmcmic@microsoft.com>
2023-02-05 17:53:48 -0500

    UPdated

--- Test-O365Recipient.ps1
+++ Test-O365Recipient.ps1
@@ -133,7 +133,7 @@
 
             try
             {
-                $functionTest = get-azureADGroup -filter "`"onPremisesSecurityIdentifier eq $member.GUID`""
+                $functionTest = get-azureADGroup -filter "onPremisesSecurityIdentifier eq $member.GUID"
 
                 $member.externalDirectoryObjectID = ("User_"+$functionTest.ObjectId)
                 $member.alias = $functionTest.mailNickName
