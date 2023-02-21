338fb492d0fd5ea0436c666451e124b64074de51
timmcmic <timmcmic@microsoft.com>
2023-02-20 21:32:56 -0500

    Updating

--- get-DLHealthReport.ps1
+++ get-DLHealthReport.ps1
@@ -3827,7 +3827,7 @@ th {
     $params = @{
                     'PreContent'='<h2>&diams;<a href="https://timmcmic.wordpress.com">Blog Information</a></h2>'}
 
-    $html_blog = ConvertTo-EnhancedHTMLFragment -InputObject $functionObject @params
+    $html_blog = ConvertTo-EnhancedHTMLFragment @params
 
     $htmlSections += $html_blog
 
