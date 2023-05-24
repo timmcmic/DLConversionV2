196c58fc722fcd48e5be500a9333ba25f1029315
timmcmic <timmcmic@microsoft.com>
2023-05-24 09:14:36 -0400

    Updating

--- Test-PowershellVersion.ps1
+++ Test-PowershellVersion.ps1
@@ -27,12 +27,12 @@
         $functionPowerShellVersion = $PSVersionTable.PSVersion
 
         out-logfile -string "Determining powershell version."
-        out-logfile -string ("Major: "+$powershellVersion.major)
-        out-logfile -string ("Minor: "+$powershellVersion.minor)
-        out-logfile -string ("Patch: "+$powershellVersion.patch)
+        out-logfile -string ("Major: "+$functionPowerShellVersion.major)
+        out-logfile -string ("Minor: "+$functionPowerShellVersion.minor)
+        out-logfile -string ("Patch: "+$functionPowerShellVersion.patch)
         out-logfile -string $functionPowerShellVersion
 
-        if ($powerShellVersion.Major -ge 7)
+        if ($functionPowerShellVersion.Major -ge 7)
         {
             out-logfile -string "Powershell 7 and higher is currently not supported due to module compatability issues."
             out-logfile -string "Please run module from Powershell 5.x"
