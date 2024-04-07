8a1d9d6a48f3e2224e86ff45d8504a6bb5822425
Timothy McMichael <timmcmic@microsoft.com>
2024-04-07 13:46:37 -0400

    Updating

--- test-nonSyncOU.ps1
+++ test-nonSyncOU.ps1
@@ -111,7 +111,6 @@
             else 
             {
                 $returnData +=("Applied synchronization settings files successfully obtained.")
-                $returnData +- ("Applied settings files count: "+($settingsFiles.count.toString()))
             }
 
             #Take the first settings file entry and utilize this as the settings file for review.
