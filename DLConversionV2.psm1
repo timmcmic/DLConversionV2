e6a36d74db20eda9590b27a993c67a794da00c58
Timothy McMichael <timmcmic@microsoft.com>
2024-04-16 08:30:02 -0400

    Updating

--- DLConversionV2.psm1
+++ DLConversionV2.psm1
@@ -292,8 +292,8 @@ Function Start-DistributionListMigration
         [Parameter(Mandatory = $true)]
         [pscredential]$activeDirectoryCredential,
         [Parameter(Mandatory = $false)]
-        [ValidateSet("Basic","Kerberos")]
-        $activeDirectoryAuthenticationMethod="Kerberos",
+        [ValidateSet("Basic","Negotiate")]
+        $activeDirectoryAuthenticationMethod="Negotiate",
         #Azure Active Directory Connect Parameters
         [Parameter(Mandatory = $false)]
         [string]$aadConnectServer=$NULL,
