b208f2aa95ba3b2a2a2256b865a5e2b392d27e8f
timmcmic <42809081+timmcmic@users.noreply.github.com>
2024-04-17 14:34:06 +0000

    Quick start (#168)
    * Updating
    
    * Updating
    
    * Updating
    
    * Updating

---
+++ show-SampleMigrationScript.ps1
@@ -0,0 +1,31 @@
+#This function opens the current quick start guide for the version of DL Conversion V2 loaded.
+
+function show-SampleMigrationScript
+{
+    #First - determine where the module is installed.
+
+    $moduleName = "DLConversionV2"
+    $ModulePSD1 = "DLConversionV2.psd1"
+    $quickStartName = "SampleMigrationScript.txt"
+
+    write-host ("Module Name: "+$moduleName)
+
+    $availableModules=@()
+
+    $availableModules += get-module $moduleName -listAvailable | sort-object Version -Descending
+
+    foreach ($module in $availableModules)
+    {
+        write-host ("Module Version: "+$module.version + " Module Path: "+$module.path )
+    }
+
+    $modulePath = $availableModules[0].path
+
+    write-host $modulePath
+
+    $quickStartPath = $modulePath.replace($modulePSD1,$quickStartName)
+
+    write-host $quickStartPath
+
+    Invoke-Item $quickStartPath
+}
