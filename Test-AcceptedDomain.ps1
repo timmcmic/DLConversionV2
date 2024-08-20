fe9cc432c004952ed3bd9e5e946c84cfec9a9bd4
Timothy McMichael <timmcmic@microsoft.com>
2024-08-20 16:48:35 -0400

    Updating

--- Test-AcceptedDomain.ps1
+++ Test-AcceptedDomain.ps1
@@ -19,7 +19,9 @@
         Param
         (
             [Parameter(Mandatory = $true)]
-            $originalDLConfiguration
+            $originalDLConfiguration,
+            [Parameter(Mandatory = $true)]
+            [boolean]$isCloudTest=$TRUE
         )
 
         $functionOnMicrosoftDomain = ""
@@ -40,62 +42,79 @@
         Out-LogFile -string "BEGIN Test-AcceptedDomain"
         Out-LogFile -string "********************************************************************************"
 
-        foreach ($address in $originalDLConfiguration.proxyAddresses)
+        if ($isCloudTest -eq $TRUE)
         {
-            Out-logfile -string "Testing proxy address for SMTP"
-            out-logfile -string $address
-
-            if ($address -like "smtp*")
+            foreach ($address in $originalDLConfiguration.proxyAddresses)
             {
-                out-logfile -string ("Address is smtp address: "+$address)
+                Out-logfile -string "Testing proxy address for SMTP"
+                out-logfile -string $address
 
-                $tempAddress=$address.split("@")
+                if ($address -like "smtp*")
+                {
+                    out-logfile -string ("Address is smtp address: "+$address)
 
-                $originalDLDomainNames+=$tempAddress[1]
+                    $tempAddress=$address.split("@")
+
+                    $originalDLDomainNames+=$tempAddress[1]
+                }
+                else 
+                {
+                    out-logfile -string ("Address is not an SMTP Address - skip.")
+                }
             }
-            else 
+
+            #It is possible that the group does not have proxy address but just mail - this is now a supported scenario.
+            #To get this far the object has to have mail.
+
+            out-logfile -string ("The mail address is: "+$originalDLConfiguration.mail)
+            $tempAddress=$originalDLConfiguration.mail.split("@")
+            $originalDLDomainNames+=$tempAddress[1]
+            
+            $originalDLDomainNames=$originalDLDomainNames | select-object -Unique
+
+            out-logfile -string "Unique domain names on the group."
+            out-logfile -string $originalDLDomainNames
+
+            foreach ($domain in $originalDLDomainNames)
             {
-                out-logfile -string ("Address is not an SMTP Address - skip.")
+                out-logfile -string "Testing Office 365 for Domain Name."
+
+                if (get-o365acceptedDomain -identity $domain)
+                {
+                    out-logfile -string ("Domain exists in Office 365. "+$domain)
+                }
+                else 
+                {
+                    out-logfile -string $domain
+                    out-logfile -string "Group cannot be migrated until the domain is an accepted domain in Office 365 or removed from the group."    
+                    out-logfile -string "Email address exists on group that is not in Office 365." -isError:$TRUE
+                }
+            }
+
+            out-logfile -string "Find and return the onmicrosoft.com domain for other functions."
+
+            try {
+                $functionOnMicrosoftDomain = get-o365acceptedDomain | where {$_.domainName -like "*$functionNameTest"} -errorAction Stop
+            }
+            catch {
+                out-logfile -string $_
+                out-logfile -string "Unable to capture accepted domains for interpretation -> error." -isError:$TRUE
             }
         }
-
-        #It is possible that the group does not have proxy address but just mail - this is now a supported scenario.
-        #To get this far the object has to have mail.
-
-        out-logfile -string ("The mail address is: "+$originalDLConfiguration.mail)
-        $tempAddress=$originalDLConfiguration.mail.split("@")
-        $originalDLDomainNames+=$tempAddress[1]
-        
-        $originalDLDomainNames=$originalDLDomainNames | select-object -Unique
-
-        out-logfile -string "Unique domain names on the group."
-        out-logfile -string $originalDLDomainNames
-
-        foreach ($domain in $originalDLDomainNames)
+        else 
         {
-            out-logfile -string "Testing Office 365 for Domain Name."
+            out-logfile -string "Find and return the onmicrosoft.com domain for other functions."
 
-            if (get-o365acceptedDomain -identity $domain)
-            {
-                out-logfile -string ("Domain exists in Office 365. "+$domain)
+            try {
+                $functionOnMicrosoftDomain = get-acceptedDomain | where {$_.domainName -like "*$functionNameTest"} -errorAction Stop
             }
-            else 
-            {
-                out-logfile -string $domain
-                out-logfile -string "Group cannot be migrated until the domain is an accepted domain in Office 365 or removed from the group."    
-                out-logfile -string "Email address exists on group that is not in Office 365." -isError:$TRUE
+            catch {
+                out-logfile -string $_
+                out-logfile -string "Unable to capture accepted domains for interpretation -> error." -isError:$TRUE
             }
         }
 
-        out-logfile -string "Find and return the onmicrosoft.com domain for other functions."
-
-        try {
-            $functionOnMicrosoftDomain = get-o365acceptedDomain | where {$_.domainName -like "*$functionNameTest"} -errorAction Stop
-        }
-        catch {
-            out-logfile -string $_
-            out-logfile -string "Unable to capture accepted domains for interpretation -> error." -isError:$TRUE
-        }
+        out-logfile -string $functionOnMicrosoftDomain.domainName
 
         Out-LogFile -string "END Test-AcceptedDomain"
         Out-LogFile -string "********************************************************************************"
